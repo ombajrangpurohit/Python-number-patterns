ced34ccb9e414263268e6192be9830a7f0331e56
Om Bajrang Purohit <ombajrangpurohit064@gmail.com>
2025-10-11 08:39:00 +0530

    updated

---
+++ LowPyramidNum1.py
@@ -0,0 +1,8 @@
+#Lower Pyramid Number Patterns
+rows = 5
+for i in range(rows):
+    for j in range(i):
+        print(" ", end = " ")
+    for j in range(9 - (2 * i)):
+        print(j + 1, end = " ")
+    print()
---
+++ UpPyramidNum2.py
@@ -0,0 +1,8 @@
+#Upper pyramid number pattern 
+rows = 5
+for i in range(rows):
+    for j in range(rows - i - 1):
+        print(" ", end = " ")
+    for j in range(2 * i + 1):
+        print(i + 1, end = " ")
+    print()
